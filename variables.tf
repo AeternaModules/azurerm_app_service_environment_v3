fcad662d835c828e96a425aa3501829bad39b3c5
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:50:50 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "app_service_environment_v3s" {
+  description = <<EOT
+Map of app_service_environment_v3s, attributes below
+Required:
+    - name
+    - resource_group_name
+    - subnet_id
+Optional:
+    - allow_new_private_endpoint_connections
+    - dedicated_host_count
+    - internal_load_balancing_mode
+    - remote_debugging_enabled
+    - tags
+    - zone_redundant
+    - cluster_setting (block):
+        - name (required)
+        - value (required)
+EOT
+
+  type = map(object({
+    name                                   = string
+    resource_group_name                    = string
+    subnet_id                              = string
+    allow_new_private_endpoint_connections = optional(bool, true)
+    dedicated_host_count                   = optional(number)
+    internal_load_balancing_mode           = optional(string, "None")
+    remote_debugging_enabled               = optional(bool, false)
+    tags                                   = optional(map(string))
+    zone_redundant                         = optional(bool, false)
+    cluster_setting = optional(object({
+      name  = string
+      value = string
+    }))
+  }))
+}
+
